232f00f934d634c6aede9b59e44321f34da5f774
dmenendez1404 <dmenendez1404@gmail.com>
2019-11-07 09:57:32 -0500

    conectando a la db sqlite

--- BackendACRP/settings.py
+++ BackendACRP/settings.py
@@ -105,14 +105,22 @@ WSGI_APPLICATION = 'BackendACRP.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'HOST': '192.168.99.100',
+#         'PORT': '5432',
+#     }
+# }
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': '192.168.99.100',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
